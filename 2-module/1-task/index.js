ae788737949971a04c22c99df23a6c76b03b4984
Vitaly <vitaly.inf@yandex.ru>
2020-10-19 20:27:37 +0300

    Исправил замечания по 1-ой задаче

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,11 +5,9 @@
  */
 function sumSalary(salaries) {
   let sum = 0;
-  for (let key in salaries){
-    if (typeof salaries[key] == 'number'){
+  for (let key in salaries) {
+    if (typeof salaries[key] == "number") {
       sum = sum + salaries[key];
-    } else{
-      false;
     }
   }
   return sum;
